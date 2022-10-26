19a87ce5e493bdbee059e85a8a4225f19207cb5f
speranza0 <speranza0p@gmail.com>
2022-10-26 15:12:30 +0900

    fix : 로그인 필터를 인터셉터로 교체

--- src/main/java/com/board/webmvc/config/WebConfig.java
+++ src/main/java/com/board/webmvc/config/WebConfig.java
@@ -2,16 +2,20 @@ package com.board.webmvc.config;
 
 import com.board.webmvc.filter.LogFilter;
 import com.board.webmvc.filter.LoginCheckFilter;
+import com.board.webmvc.interceptor.LogInterceptor;
+import com.board.webmvc.interceptor.LoginCheckInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    //@Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
@@ -20,7 +24,20 @@ public class WebConfig {
         return filterRegistrationBean;
     }
 
-    @Bean
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/assets/*", "/script/*", "/style/*", "/*.ico", "/error");
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/user/*", "/board/list", "/board/detail", "/board/attachFile", "/assets/*", "/script/*", "/style/*", "/*.ico", "/error");
+    }
+
+    //@Bean
     public FilterRegistrationBean loginCheckFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LoginCheckFilter());
